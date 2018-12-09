33afe94c2f218b7eb8aa352eecbee03b64820a1f
praewiiezzz <chutikan_praew@hotmail.com>
2018-12-10 01:15:14 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -29,7 +29,7 @@ if (!is_null($events['events'])) {
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
-			/*$url = 'https://api.line.me/v2/bot/message/reply';
+			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
 				'messages' => [$messages],
@@ -44,20 +44,11 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
-			curl_close($ch);*/
+			curl_close($ch);
 
 			echo $result . "\r\n";
 		}
 	}
 }
-require "vendor/autoload.php";
-$access_token = 'C6yU4avsrRJwRDd1QLE5GMjfW4FKaEVkE0cuCdCPMSpwPFkhxy6DPiT4BJt+LXH0Krw7P105yBcorxE0TzT1mlmvvTWzTOk3tAt6av5wjzkNQNddBAgFejnTYcSj66J+i40XZuUSyIyys5xX+jC+ewdB04t89/1O/w1cDnyilFU=';
-$channelSecret = '17fece9503cef636192da92fb566ee4d';
-$pushID = 'U5479bc5c09c356cdec1a1d1b36a5d9e0';
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text2);
-$response = $bot->pushMessage($pushID, $textMessageBuilder);
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 echo "ok";
 echo "OK";
