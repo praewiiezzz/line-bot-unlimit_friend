e7c8664afad231293629433562cee7d72f7035a9
praewiiezzz <chutikan_praew@hotmail.com>
2019-01-14 01:34:56 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -29,7 +29,7 @@ if (!is_null($events['events'])) {
 			];
 			
 			// Make a POST Request to Messaging API to reply to sender
-			/*$url = 'https://api.line.me/v2/bot/message/reply';
+			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
 				'messages' => [$messages],
@@ -44,7 +44,7 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
-			curl_close($ch);*/
+			curl_close($ch);
 
 			echo $result . "\r\n";
 		}
